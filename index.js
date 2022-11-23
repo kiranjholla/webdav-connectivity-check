b524d0d1a660f84c72cb92234d011316b62c583b
KH <4491818+kiranjholla@users.noreply.github.com>
2022-11-23 16:25:19 +0530

    Implementing promise chaining instead of callbacks

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const prompt = require('prompt');
 
-const yo = prompt.get(
-  {
+prompt
+  .get({
     properties: {
       username: { description: 'Please enter the username' },
       password: { description: 'Please enter the password', hidden: true }
     }
-  },
-  (error, { username, password }) => {
-    if (!error) {
-      console.log({ username, password });
-    } else {
-      console.log('There was an error: ', error);
-    }
-  }
-);
+  })
+  .then(({ username, password }) => {
+    console.log({ username, password });
+  })
+  .catch(error => console.log('There was an error: ', error));
