acfdb2333c7ded5e2c246d86a77fb3b82b25b752
KH <4491818+kiranjholla@users.noreply.github.com>
2022-11-23 16:40:53 +0530

    Connect to WebDAV and fetch results

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-import prompt from 'prompt'
+import prompt from 'prompt';
+import { createClient, AuthType } from 'webdav';
 
 prompt
   .get({
     properties: {
+      webdav: { description: 'Please enter the WebDAV URL' },
       username: { description: 'Please enter the username' },
       password: { description: 'Please enter the password', hidden: true }
     }
   })
-  .then(({ username, password }) => {
-    console.log({ username, password });
+  .then(({ webdav, username, password }) => {
+    console.log(`Connecting to: ${webdav}`);
+    const client = createClient(webdav, {
+      authType: AuthType.Digest,
+      username,
+      password
+    });
+
+    return client.getDirectoryContents('/');
   })
-  .catch(error => console.log('There was an error: ', error));
+  .then(x => console.log(x))
+  .catch(error => console.log('There was an error: ', error.message));
